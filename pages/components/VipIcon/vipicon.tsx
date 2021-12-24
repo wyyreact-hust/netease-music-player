4dfa7e2fb6371040195242632689cd7f3bb36ab4
KiritoKing <kiritoclzh@gmail.com>
2021-12-24 16:26:48 +0800

    commit

---
+++ pages/components/VipIcon/vipicon.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import styles from "./vipicon.module.css"
+
+const VipIcon = () => (<div className={styles.root}>VIP</div>)
+export default VipIcon;
---
+++ pages/hooks/test.ts
@@ -0,0 +1,1 @@
+import { useCallback, useState } from 'react'
